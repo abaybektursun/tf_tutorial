b378f7cdf3a9036d4eefe32da8d792b3af6d10c2
Abay Bektursun <abaybektursun@gmail.com>
2018-11-20 18:01:01 -0600

    sync

--- crop_dataset.sh
+++ crop_dataset.sh
@@ -1,10 +1,13 @@
-for N in {1..4}; do \
+for N in {1..2}; do
   python3 detector/align_dataset_mtcnn.py \
     $PWD/data/dataset/raw \
     $PWD/data/dataset/cropped \
   --image_size 160 \
   --margin 32 \
   --random_order \
-  --gpu_memory_fraction 0.25 \
-& done \
-&& python3 split_dataset.py
+  --gpu_memory_fraction 0.25 
+done 
+
+echo "Splitting the dataset"
+python3 split_dataset.py
+
